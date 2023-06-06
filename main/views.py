938b52eb24faa681fed3d59bd492bdab0524f609
Alexandr <sanchous.vas@gmail.com>
2023-06-06 23:02:55 +0300

    added posts in template index.html + auth in base.html

--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Post
 
 
 def index(request):
     categories = Category.objects.all()
+    posts = Post.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'posts': posts,
     }
     return render(request, 'index.html', context)
 
