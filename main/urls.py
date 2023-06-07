2e68ca51f9506bb50f6576da8ac1afd9014b76e7
Alexandr <sanchous.vas@gmail.com>
2023-06-07 20:50:41 +0300

    modified index page for ajax, get requests

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import index, posts_by_page
+from .views import index
 
 app_name = 'main'
 
 
 urlpatterns = [
     path('', index, name='home'),
-    path('pbp', posts_by_page, name='pbp'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,23 +4,27 @@ from .models import Category, Post
 
 
 def index(request):
-    categories = Category.objects.all()
+    PER_PAGE = 6
+    category = request.GET.get('category')
     all_posts = Post.objects.all()
-    paginator = Paginator(all_posts, 6)  # 6 элементов на странице
-    page = paginator.get_page(1)  # Первая страница по умолчанию
-    context = {
-        'page': page,
-        'categories': categories,
-        'posts': page.object_list,
-    }
-    return render(request, 'index.html', context)
 
+    if category:
+        all_posts = all_posts.filter(category__slug=category)
 
-def posts_by_page(request):
-    all_posts = Post.objects.all()
-    paginator = Paginator(all_posts, 6)  # Показывать по 6 постов на каждой странице
-    page_number = request.GET.get('page', 2)  # Получаем номер страницы из параметров запроса
-    page = paginator.get_page(page_number)  # Получаем объект страницы
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        paginator = Paginator(all_posts, PER_PAGE)
+        page_number = request.GET.get('page', 2)  # Получаем номер страницы из параметров запроса
+        page = paginator.get_page(page_number)  # Получаем объект страницы
 
-    return render(request, 'pbp.html', {'page': page, 'posts': page.object_list})
+        return render(request, 'index-ajax.html', {'page': page, 'posts': page.object_list})
+    else:
+        categories = Category.objects.all()
+        paginator = Paginator(all_posts, PER_PAGE)
+        page = paginator.get_page(1)  # Первая страница по умолчанию
+        context = {
+            'page': page,
+            'categories': categories,
+            'posts': page.object_list,
+        }
+        return render(request, 'index.html', context)
 
